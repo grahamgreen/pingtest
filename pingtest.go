3e42ba6a891d200b1fccdd0ed1fc471100911725
Graham Green <grahamgreen@gmail.com>
2015-08-13 22:03:41 -0500

    moved to rrd files

--- pingtest.go
+++ pingtest.go
@@ -11,183 +11,185 @@ import (
 	"tatsushid/go-fastping"
 	"time"
 
+	"github.com/codegangsta/cli"
 	"github.com/grahamgreen/goutils"
-	"github.com/segmentio/go-loggly"
+	"github.com/ziutek/rrd"
 )
 
-//just some text
+const (
+	version   = "0.1.0"
+	rrd_dir   = "/home/ggreen/"
+	step      = 1
+	heartbeat = 2 * step
+)
 
 type response struct {
 	addr *net.IPAddr
 	rtt  time.Duration
 }
 
-type RunningAvg struct {
-	avg   time.Duration
-	count int64
-}
-
-func (avg *RunningAvg) UpdateAvg(val time.Duration) {
-	newAvg := ((avg.avg.Nanoseconds() * avg.count) + val.Nanoseconds()) / (avg.count + 1)
-	avg.avg = time.Duration(newAvg)
-	avg.count++
-}
-
-type Updateable interface {
-	UpdateAvg(val time.Duration)
-}
-
-func UpdateAvg(v Updateable, val time.Duration) {
-	v.UpdateAvg(val)
-}
-
 type host struct {
-	name  string
-	ip    net.IP
-	fails uint64
-	avg   RunningAvg
+	name    string
+	ip      net.IP
+	fails   uint64
+	rrdFile string
 }
+
+func BuildRRD(dbfile string, overwrite bool) error {
+	now := time.Now()
+	start := now.Add(-5 * time.Second)
+
+	c := rrd.NewCreator(dbfile, start, step)
+	c.DS("rtt", "GAUGE", heartbeat, "U", "U")
+	c.DS("fail", "COUNTER", heartbeat, "U", "U")
+	c.RRA("AVERAGE", 0.5, 1, 300)    //5min w/ sec res
+	c.RRA("AVERAGE", 0.5, 10, 90)    //10min w/ sec res
+	c.RRA("AVERAGE", 0.5, 60, 60)    //1h w/ min res
+	c.RRA("AVERAGE", 0.5, 60, 360)   //6h
+	c.RRA("AVERAGE", 0.5, 60, 720)   //12h
+	c.RRA("AVERAGE", 0.5, 60, 1440)  //24h
+	c.RRA("AVERAGE", 0.5, 3600, 168) //1w w/ hour res
+	c.RRA("AVERAGE", 0.5, 3600, 744) //1month
+	err := c.Create(overwrite)
+	return err
+}
+
+//func BuildGraph(title string) *rrd.Grapher {
+//	g := rrd.NewGrapher()
+//	g.SetTitle(title)
+//	g.SetSize(750, 300)
+//	g.Def("1", dbfile, "1", "AVERAGE")
+//	g.Def("2", dbfile, "2", "AVERAGE")
+//	g.Line(2, "1", "ff0000", "597")
+//	g.Line(2, "2", "00ff00", "555")
+//
+//	return g
+//}
 
 func main() {
-	logToken := os.Getenv("LOGTOKEN")
-	goutils.NotEmpty(logToken)
-	logs := loggly.New(logToken)
-	logs.FlushInterval = 30 * time.Second
-
-	f, err := os.OpenFile("/home/ggreen/tmp/pingtest/pingfail.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		fmt.Errorf("error opening file: %v", err)
+	app := cli.NewApp()
+	app.Version = version
+	app.Name = "PingTest"
+	app.Usage = "testing the pings"
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "overwrite",
+			Usage: "Overwrite the RRD file(s) if they exist Default: False",
+		},
 	}
-	defer f.Close()
+	app.Action = func(context *cli.Context) {
+		ticker5 := time.NewTicker(5 * time.Second)
+		overwrite := context.GlobalBool("overwrite")
 
-	log.SetOutput(f)
-	log.Println("Starting PingTest")
-
-	hosts := make(map[string]*host)
-	for _, val := range os.Args[1:] {
-		split := strings.Split(val, ":")
-		theHost := host{}
-		if len(split) == 1 {
-			theHost = host{name: split[0], ip: net.ParseIP(split[0])}
-		} else {
-			theHost = host{name: split[0], ip: net.ParseIP(split[1])}
+		f, err := os.OpenFile("/home/ggreen/tmp/pingtest/pingfail2.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			fmt.Errorf("error opening file: %v", err)
 		}
-		hosts[theHost.ip.String()] = &theHost
-	}
+		defer f.Close()
 
-	results := make(map[string]*response)
+		log.SetOutput(f)
+		log.Println("Starting PingTest")
 
-	rttChan := make(chan *response, 6)
-	failChan := make(chan *host, 6)
+		hosts := make(map[string]*host)
+		for _, val := range context.Args() {
+			split := strings.Split(val, ":")
+			theHost := host{}
+			if len(split) == 1 {
+				theHost = host{name: split[0], ip: net.ParseIP(split[0])}
+			} else {
+				theHost = host{name: split[0], ip: net.ParseIP(split[1])}
+			}
 
-	go func() {
+			hosts[theHost.ip.String()] = &theHost
+			theHost.rrdFile = fmt.Sprintf("%s%s.rrd", rrd_dir, theHost.name)
+			goutils.Check(BuildRRD(theHost.rrdFile, overwrite))
+		}
+
+		results := make(map[string]*response)
+
+		rttChan := make(chan *response, 6)
+		failChan := make(chan *host, 6)
+
+		go func() {
+			for {
+				select {
+				case res := <-rttChan:
+					if host, ok := hosts[res.addr.String()]; ok {
+						dbfile := fmt.Sprintf("%s%s.rrd", rrd_dir, host.name)
+						u := rrd.NewUpdater(dbfile)
+						err := u.Update(time.Now(), res.rtt.Nanoseconds(), 0)
+						goutils.Check(err)
+					}
+				case host := <-failChan:
+					host.fails++
+					dbfile := fmt.Sprintf("%s%s.rrd", rrd_dir, host.name)
+					u := rrd.NewUpdater(dbfile)
+					err := u.Update(time.Now(), 0, 1)
+					goutils.Check(err)
+				case <-ticker5.C:
+					fmt.Println("ticker5")
+				}
+			}
+		}()
+
+		pinger := fastping.NewPinger()
+
+		for _, aHost := range hosts {
+			addr, err := net.ResolveIPAddr("ip4:icmp", aHost.ip.String())
+			goutils.Check(err)
+			results[addr.String()] = nil
+			pinger.AddIPAddr(addr)
+		}
+
+		onRecv, onIdle := make(chan *response), make(chan bool)
+		pinger.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
+			onRecv <- &response{addr: addr, rtt: rtt}
+		}
+		pinger.OnIdle = func() {
+			onIdle <- true
+		}
+		pinger.MaxRTT = time.Second
+		pinger.RunLoop()
+
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+		signal.Notify(c, syscall.SIGTERM)
+
+	loop:
 		for {
 			select {
-			case res := <-rttChan:
-				if host, ok := hosts[res.addr.String()]; ok {
-					UpdateAvg(&host.avg, res.rtt)
-					msg := loggly.Message{
-						"timestamp": time.Now().Format(time.RFC3339),
-						"name":      host.name,
-						"avg":       host.avg,
-						"rtt":       res.rtt,
+			case <-c:
+				fmt.Println("got interrupted")
+				log.Println("Stopping PingTest")
+				log.Println("Fail Count")
+				for _, host := range hosts {
+					log.Printf("%s : %d\n", host.name, host.fails)
+				}
+				break loop
+			case res := <-onRecv:
+				if _, ok := results[res.addr.String()]; ok {
+					results[res.addr.String()] = res
+				}
+			case <-onIdle:
+				for hostIP, r := range results {
+					if r == nil {
+						log.Printf("%v, %s, %s\n", time.Now().Format(time.RFC3339), hosts[hostIP].name, hostIP)
+						failChan <- hosts[hostIP]
+					} else {
+						rttChan <- r
 					}
-					logs.Info("sucess", msg)
+					results[hostIP] = nil
 				}
-			case host := <-failChan:
-				host.fails++
-				msg := loggly.Message{
-					"timestamp": time.Now().Format(time.RFC3339),
-					"name":      host.name,
-					"avg":       host.avg,
-					"failcount": host.fails,
+			case <-pinger.Done():
+				if err := pinger.Err(); err != nil {
+					fmt.Println("ping failed:", err)
 				}
-				logs.Error("fail", msg)
+				break loop
 			}
 		}
-	}()
-
-	pinger := fastping.NewPinger()
-
-	for _, aHost := range hosts {
-		addr, err := net.ResolveIPAddr("ip4:icmp", aHost.ip.String())
-		goutils.Check(err)
-		results[addr.String()] = nil
-		pinger.AddIPAddr(addr)
+		signal.Stop(c)
+		pinger.Stop()
 	}
 
-	onRecv, onIdle := make(chan *response), make(chan bool)
-	pinger.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
-		onRecv <- &response{addr: addr, rtt: rtt}
-	}
-	pinger.OnIdle = func() {
-		onIdle <- true
-	}
-	pinger.MaxRTT = time.Second
-	pinger.RunLoop()
-
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	signal.Notify(c, syscall.SIGTERM)
-
-loop:
-	for {
-		select {
-		case <-c:
-			fmt.Println("got interrupted")
-			log.Println("Stopping PingTest")
-			log.Println("RTT AVG")
-			for _, host := range hosts {
-				fmt.Printf("%s : %v\n", host.name, host.avg.avg)
-				log.Printf("%s : %v\n", host.name, host.avg.avg)
-			}
-			log.Println("Fail Count")
-			for _, host := range hosts {
-				fmt.Printf("%s : %d\n", host.name, host.fails)
-				log.Printf("%s : %d\n", host.name, host.fails)
-			}
-			break loop
-		case res := <-onRecv:
-			if _, ok := results[res.addr.String()]; ok {
-				results[res.addr.String()] = res
-			}
-		case <-onIdle:
-			for hostIP, r := range results {
-				if r == nil {
-					fmt.Printf("%v %s : unreachable\n", time.Now().Format(time.RFC3339), hosts[hostIP].name)
-					log.Printf("%v, %s, %s\n", time.Now().Format(time.RFC3339), hosts[hostIP].name, hostIP)
-					failChan <- hosts[hostIP]
-				} else {
-					fmt.Printf("time:%v,name:%v,totalAvg:%v,rtt:%v\n", time.Now().Format(time.RFC3339), hosts[hostIP].name, hosts[hostIP].avg.avg, r.rtt)
-					rttChan <- r
-				}
-				results[hostIP] = nil
-			}
-		case <-pinger.Done():
-			if err := pinger.Err(); err != nil {
-				fmt.Println("ping failed:", err)
-			}
-			break loop
-		}
-	}
-	signal.Stop(c)
-	pinger.Stop()
+	app.Run(os.Args)
 }
-
-//p := fastping.NewPinger()
-//ra, err := net.ResolveIPAddr("ip4:icmp", os.Args[1])
-//if err != nil {
-//	fmt.Println(err)
-//	os.Exit(1)
-//}
-//p.AddIPAddr(ra)
-//p.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
-//	fmt.Printf("IP Addr: %s receive, RTT: %v\n", addr.String(), rtt)
-//}
-//p.OnIdle = func() {
-//	fmt.Println("finish")
-//}
-//err = p.Run()
-//if err != nil {
-//	fmt.Println(err)
-//}
